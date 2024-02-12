af17b7d5478939c2e9b5fdeede7fa0630cb81fd6
Jeremy <jspaeth@prevor.com>
2024-02-12 22:17:23 +0100

    On create user, the password is now encrypted

--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
@@ -11,10 +14,17 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 
 class UtilisateurCrudController extends AbstractCrudController
 {
+    public function __construct(public UserPasswordHasherInterface $userPasswordHasher)
+    {
+
+    }
     public static function getEntityFqcn(): string
     {
         return Utilisateur::class;
@@ -27,7 +37,10 @@ class UtilisateurCrudController extends AbstractCrudController
             TextField::new('prenom'),
             TextField::new('nom'),
             TextField::new('identifiant'),
-            TextField::new('password')->setFormType(PasswordType::class)->hideOnIndex(),
+            TextField::new('password')
+                ->setFormType(PasswordType::class)
+                ->setRequired($pageName === Crud::PAGE_NEW)
+                ->onlyOnForms(),
             EmailField::new('email'),
             ArrayField::new('roles'),
             BooleanField::new('actif')->setValue(true),
@@ -38,4 +51,36 @@ class UtilisateurCrudController extends AbstractCrudController
         ];
     }
 
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword() {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->userPasswordHasher->hashPassword($this->getUser(), $password);
+            $form->getData()->setPassword($hash);
+        };
+    }
 }
