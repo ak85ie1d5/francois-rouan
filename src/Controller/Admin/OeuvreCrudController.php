c822597bc786fbd30305ed2689a3e840bfe7f6ee
Jeremy <jspaeth@prevor.com>
2024-02-17 14:27:14 +0100

    Configuring the field to display edit **Oeuvres**

--- src/Controller/Admin/OeuvreCrudController.php
+++ src/Controller/Admin/OeuvreCrudController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Oeuvre;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Ranky\MediaBundle\Presentation\Form\EasyAdmin\EARankyMediaFileManagerField;
 
@@ -21,15 +24,33 @@ class OeuvreCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            FormField::addTab('Général'),
+            FormField::addColumn('col-lg-6'),
             TextField::new('numInventaire', 'N°inv'),
             TextField::new('titre'),
+            TextField::new('sousTitre'),
+            TextareaField::new('serie', 'Série')->stripTags(),
             DateField::new('date'),
-            TextField::new('dateComplement'),
-            TextEditorField::new('description'),
+            TextField::new('dateComplement', 'Complément de date'),
+            TextField::new('dimensions'),
+            TextareaField::new('description')->stripTags(),
+            FormField::addColumn('col-lg-6'),
+            TextareaField::new('commentairePublic', 'Commentaire public')->stripTags(),
+            TextareaField::new('commentaireInterne', 'Commentaire interne')->stripTags(),
+            AssociationField::new('categorie', 'Catégorie'),
+            IntegerField::new('sousCategorie', 'Sous catégorie'),
+            TextareaField::new('details', 'Details')->stripTags(),
+
+            FormField::addTab('Historique'),
+            FormField::addTab('Bibliographie'),
+            FormField::addTab('Exposition'),
+            FormField::addTab('Localisation'),
+            FormField::addTab('Médias'),
             EARankyMediaFileManagerField::new('media')
                 ->multipleSelection()
                 ->savePath(true)
-                ->modalTitle('Galerie')
+                ->modalTitle('Galerie'),
+
         ];
     }
 
