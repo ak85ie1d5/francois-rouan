1e67ece856f33dd0e00dd033868ede801e080125
Jeremy <jspaeth@prevor.com>
2024-04-25 15:41:49 +0200

    Adding Trait to manage fields updateAt and CreatedAt in entities

--- src/Entity/ArtworkCategory.php
+++ src/Entity/ArtworkCategory.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\TimeColumnTrait;
 use App\Repository\ArtworkCategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Umanit\EasyAdminTreeBundle\Entity\AbstractTreeItem;
 
 #[ORM\Entity(repositoryClass: ArtworkCategoryRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class ArtworkCategory extends AbstractTreeItem
 {
-
+    use TimeColumnTrait;
 }
