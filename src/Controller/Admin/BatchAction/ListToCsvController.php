611fbd448e7f8adb87742a5a04fa1fe74cd5ef95
Jeremy <jspaeth@prevor.com>
2025-01-14 20:37:49 +0100

    Fix: Export to CSV batch action use now ID from cookie instead of 'BatchActionDto' class

--- src/Controller/Admin/BatchAction/ListToCsvController.php
+++ src/Controller/Admin/BatchAction/ListToCsvController.php
@@ -13,9 +13,13 @@ class ListToCsvController extends AbstractController
 {
     private CsvExportService $csvExportService;
 
+    private mixed $selectedArtworks;
+
     public function __construct(CsvExportService $csvExportService)
     {
         $this->csvExportService = $csvExportService;
+
+        $this->selectedArtworks = isset($_COOKIE['selectedArtworks']) ? json_decode($_COOKIE['selectedArtworks'], true) : [];
     }
 
     #[Route('/list/to/csv', name: 'app_list_to_csv')]
@@ -24,7 +28,7 @@ class ListToCsvController extends AbstractController
         $ids = $batchActionDto->getEntityIds();
 
         $fields = ["numInventaire", "titre", "FirstMonth", "FirstYear", "SecondMonth", "SecondYear", "dimensions", "description", "commentairePublic", "lastLocalisation"];
-        $csvContent = $this->csvExportService->generateCsv($ids, $fields);
+        $csvContent = $this->csvExportService->generateCsv($this->selectedArtworks, $fields);
 
         $filename = 'export_oeuvres_'.date('Y-m-d').'.csv';
         $response = new Response($csvContent);
