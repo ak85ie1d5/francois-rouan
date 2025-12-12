15b0a5a79a84fa6f59877da1e3cba93f4efe5146
Jeremy SPAETH <ak85ie1d5@relay.firefox.com>
2025-12-12 17:41:57 +0100

    Update Command name from property to annotation

--- src/Command/GenerateThumbnailsCommand.php
+++ src/Command/GenerateThumbnailsCommand.php
@@ -6,16 +6,19 @@ namespace App\Command;
 use App\Entity\ArtworkMedia;
 use App\Service\ThumbnailListener;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Vich\UploaderBundle\Mapping\PropertyMappingFactory;
 use Vich\UploaderBundle\Event\Event;
 
+#[AsCommand(
+    name: 'app:generate-thumbnails',
+    description: 'Generates thumbnails for all existing images.',
+)]
 class GenerateThumbnailsCommand extends Command
 {
-    protected static $defaultName = 'app:generate-thumbnails';
-
     private $entityManager;
     private $thumbnailListener;
     private $propertyMappingFactory;
