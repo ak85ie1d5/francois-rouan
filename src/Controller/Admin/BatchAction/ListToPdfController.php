c501f682f950bd72a3e89bb39f8b5f6d661291ba
Jeremy SPAETH <ak85ie1d5@relay.firefox.com>
2026-04-13 10:41:18 +0200

    Remove old condition on export to PDF with external location

--- src/Controller/Admin/BatchAction/ListToPdfController.php
+++ src/Controller/Admin/BatchAction/ListToPdfController.php
@@ -96,11 +96,6 @@ class ListToPdfController extends AbstractController
                 $fields['exhibitions'] = $this->pdfExportService->getExhibition($id);
             }
 
-            if ($request->request->get('includeExternalLocation') === 'true') {
-                // Retrieve the last localization of the Oeuvre entity
-                $fields['last_localisation'] = $this->entityManager->getRepository(Oeuvre::class)->getLastLocalisation($id);
-            }
-
             if ($request->request->get('includeExternalLocation') === 'true' || $request->request->get('includeInternalLocation') === 'true') {
                 // Retrieve the last localization of the Oeuvre entity
                 $fields['last_localisation'] = $this->entityManager->getRepository(Oeuvre::class)->getMultipleLastLocalisation([$id])[$id];
