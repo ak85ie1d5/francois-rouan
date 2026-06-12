5133e8c2ebe28c40bc320b4c917862f9faac3a08
Jeremy SPAETH <ak85ie1d5@relay.firefox.com>
2026-06-12 14:58:04 +0200

    Fix error when getType() methode return null

--- src/Controller/Admin/OeuvreCrudController.php
+++ src/Controller/Admin/OeuvreCrudController.php
@@ -371,13 +371,13 @@ class OeuvreCrudController extends AbstractCrudController
             TableField::new('oeuvreStockages', 'Dernière localisation ')
                 ->setEntryType(StockageCollectionType::class)
                 ->formatValue(function ($value, $entity) {
-                    if ($entity->getOeuvreStockages()[0]->getType() === 0) {
+                    if ($entity->getOeuvreStockages()[0]?->getType() === 0) {
                         if ($entity->getOeuvreStockages()[0]->getInternalLocation() === null) {
                             return "Atelier de l'artiste";
                         } else {
                             return $entity->getOeuvreStockages()[0]->getInternalLocation();
                         }
-                    } elseif ($entity->getOeuvreStockages()[0]->getType() === 1) {
+                    } elseif ($entity->getOeuvreStockages()[0]?->getType() === 1) {
                         return $entity->getOeuvreStockages()[0]->getLieu();
                     } else {
                         return '';
